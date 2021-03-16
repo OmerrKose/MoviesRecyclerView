4548a122eb7edc1122ff8a77766c5b3576802c83
ogulcan <ogulcanucarsu@gmail.com>
2021-03-16 12:14:36 +0300

    bug fixed

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,8 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rv = findViewById<RecyclerView>(R.id.recyclerView)
-        rv.LayoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        RecyclerView.Adapter = Adapter(getModels())
+        val moviesAdapter = Adapter(getModels())
+        rv.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+            adapter = moviesAdapter
+        }
+
     }
     private fun getModels(): MutableList<Movies> {
         return mutableListOf(
